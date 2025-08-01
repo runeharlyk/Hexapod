e2dc577a0a8981888a6112d4056cf00b9e8dfb6e
Rune Harlyk <rune@harlyk.dk>
2025-08-01 20:11:17 +0200

    🎨 Cleans up servo UI

--- app/src/routes/peripherals/servo/servos.svelte
+++ app/src/routes/peripherals/servo/servos.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
   import SettingsCard from '$lib/components/SettingsCard.svelte'
-  import type { ServoConfiguration, Servo } from '$lib/types/models'
   import Spinner from '$lib/components/Spinner.svelte'
-
   import { socket } from '$lib/stores'
-  import { onDestroy, onMount } from 'svelte'
   import { throttler as Throttler } from '$lib/utilities'
   import { MotorOutline } from '$lib/components/icons'
   import { dataBroker } from '$lib/transport/databroker'
@@ -18,24 +15,17 @@
 
   const throttler = new Throttler()
 
-  const sweep = (event: any) => {
-    let channel = event.detail.channel
-    socket.sendEvent('servoConfiguration', { servos: [{ channel, sweep: true }] })
-  }
-
-  const activateServo = (event: any) => {
+  const activateServo = () => {
     socket.sendEvent('servoState', { active: 1 })
   }
 
-  const deactivateServo = (event: any) => {
+  const deactivateServo = () => {
     socket.sendEvent('servoState', { active: 0 })
   }
 
-  const setServoCenter = () => {}
-
   const updatePWM = () => {
     throttler.throttle(() => {
-      dataBroker.emit(MessageTopic.SERVO, { servo_id: servoId, pwm })
+      dataBroker.emit(MessageTopic.SERVO, { id: servoId, pwm })
     }, 10)
   }
 </script>
